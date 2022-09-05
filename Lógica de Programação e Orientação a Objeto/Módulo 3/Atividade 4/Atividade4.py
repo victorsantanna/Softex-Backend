ba7588bad4d0b133e9e52c40778bc525180d9e41
Victor Santana <victor.esef@hotmail.com>
2022-09-05 20:59:21 -0300

    Módulo 3

---
+++ Lógica de Programação e Orientação a Objeto/Módulo 3/Atividade 4/Atividade4.py
@@ -0,0 +1,46 @@
+|#Crie um tipo abstrato de dado (TAD) para manipular números complexos na linguagem Python.
+#O método deve:
+
+#- calcular três números complexos;
+#- realizar todas as operações básicas;
+#- e imprimir as propriedades real e img do números. 
+
+class Complexos:
+    
+    def calcular_complexos(a, b, c):
+        N1 = complex(a)
+        N2 = complex(b, c)
+        N3 = complex(c, a)
+
+        print("Qual o primeiro número complexo ?")
+        type(a)
+
+        print("Qual o segundo número ?")
+        type(b)
+
+        print("Qual o terceiro número ?")
+        type(c)
+
+        
+        adicao = N1 + N2 + N3
+        print("Adição =", adicao)
+        print("Parte Real =", adicao.real)
+        print("Parte Imaginária =", adicao.imag)
+    
+        Sub = N1 - N2 - N3
+        print("Subtração =", sub)
+        print("Parte Real =", sub.real)
+        print("Parte Imaginária =", sub.imag)
+    
+        Mult = N1 * N2 * N3
+        print("Multiplicação =", mult)
+        print("Parte Real =", mult.real)
+        print("Parte Imaginária =", mult.imag)
+    
+        Div = N1 / N2 / N3
+        print("Divisão =", div)
+        print("Parte Real =", div.real)
+        print("Parte Imaginária =", div.imag)
+
+        
+    print(calcular_complexos(a, b, c))
---
+++ Lógica de Programação e Orientação a Objeto/Módulo 3/Atividade 6/Atividade6.md
@@ -0,0 +1,19 @@
+Crie uma árvore para cada lista abaixo, adicione um valor nela e remova outro, mas, em pelo menos uma das árvores, a remoção deve ser de um nó com dois filhos.
+
+
+Lista1 = 45,20,30,60,81,97,100,7,8,4
+
+<img src="./Slide1.JPG" alt="árvore antes"/>
+
+REMOVENDO 7 E ADICIONANDO 99 NOVA LISTA 1= 45,20,30,60,81,97,100,8,4, 99
+
+<img src="./Slide2.JPG" alt="árvore depois"/>
+
+
+Lista2 = 15,6,18,3,7,16,20,4
+
+<img src="./Slide3.JPG" alt="árvore antes"/>
+
+REMOVENDO 4 E ADICIONANDO 30 Lista2 = 15,6,18,3,7,16,20,30
+
+<img src="./Slide4.JPG" alt="árvore depois"/>
